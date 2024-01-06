33bc441deaca6e65020be586c7b5246725f69f90
Tim Borthwick <tjborthwick@gmail.com>
2024-01-06 11:09:15 -0500

    Add outline to main nav when tabbing through.

--- resources/js/Components/ResponsiveNavLink.vue
+++ resources/js/Components/ResponsiveNavLink.vue
@@ -20,8 +20,8 @@ const props = defineProps({
 
 const classes = computed(() => {
   let result = props.active
-        ? 'py-1 font-roboto font-bold text-base text-grey-400 uppercase outline-0 border-b-2 border-tan focus:border-grey-300'
-        : 'py-1 font-roboto font-bold text-base text-grey-400 uppercase outline-0 hover:border-b-2 active:border-b-2 focus:border-b-2'
+        ? 'py-1 font-roboto font-bold text-base text-grey-400 uppercase border-b-2 border-tan focus:border-grey-300'
+        : 'py-1 font-roboto font-bold text-base text-grey-400 uppercase hover:border-b-2 active:border-b-2 focus:border-b-2'
 
   result += isInverted ? ' border-white' : 'border-tan'
 
